34ae0fc48b689f28ba07d6d73ba302209a08b5c0
Vader <ipzk241_lvv@student.ztu.edu.ua>
2025-01-22 17:57:07 +0200

    First working commit

---
+++ pr1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AdditionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть перше число: ");
+            string input1 = Console.ReadLine();
+            
+            Console.Write("Введіть друге число: ");
+            string input2 = Console.ReadLine();
+
+            if (double.TryParse(input1, out double num1) && double.TryParse(input2, out double num2))
+            {
+                double sum = num1 + num2;
+                Console.WriteLine($"Сума {num1} + {num2} = {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Будь ласка, введіть коректні числа.");
+            }
+        }
+    }
+}
